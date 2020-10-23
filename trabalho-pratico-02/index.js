96d1e69b59ecc994d21e712d4be91988e2cc4c8e
ramonis567 <ramonmatosferreira@gmail.com>
2020-10-23 15:36:28 -0300

    Final Commit - TP2

--- trabalho-pratico-02/index.js
+++ trabalho-pratico-02/index.js
@@ -5,7 +5,8 @@ init();
 async function init(){
   await initData();
   await statesWithMoreandLessCities();
-  await citiesWithTheLongestName();
+  await citiesWithTheLongestandSmallerName();
+  await getTheLongestandSmallerName();
 
 }
 
@@ -55,8 +56,23 @@ async function statesWithMoreandLessCities(){
   console.log(result2)
 }
 
-async function citiesWithTheLongestName(){
+async function citiesWithTheLongestandSmallerName(){
+  const states = JSON.parse(await fs.readFile("./OriginalFiles/Estados.json"));
+  const resultList1 = [];
+  const resultList2 = [];
+  let state;
 
+  for(state of states){
+    const city1 = await getLongestName(state.Sigla);
+    const city2 = await getSmallerName(state.Sigla);
+    resultList1.push(state.Sigla + ": " + city1.Nome);
+    resultList2.push(state.Sigla + ": " + city2.Nome);
+  }
+  console.log("-- MAIORES NOMES EM CADA ESTADO");
+  console.log(resultList1);
+
+  console.log("-- MENORES NOMES EM CADA ESTADO");
+  console.log(resultList2);
 }
 
 async function getLongestName(uf){
@@ -77,4 +93,63 @@ async function getLongestName(uf){
   });
 
   return longestCity;
+}
+
+async function getSmallerName(uf){
+  const cities = JSON.parse(await fs.readFile(`./Estados/${uf}.json`));
+
+  let smallerCity;
+
+  cities.forEach(city => {
+    if(!smallerCity){
+      smallerCity = city;
+    }else if(city.Nome.length < smallerCity.Nome.length){
+      smallerCity = city;
+    }else if(city.Nome.length === smallerCity.Nome.length){
+      if(city.Nome.toLowerCase() < smallerCity.Nome.toLowerCase()){   //Compara ordem alfabetica
+        smallerCity = city;
+      }
+    }
+  });
+
+  return smallerCity;
+}
+
+async function getTheLongestandSmallerName(){
+  const states = JSON.parse(await fs.readFile("./OriginalFiles/Estados.json"));
+  let state;
+  let resultList1 = [];
+  let resultList2 = [];
+
+  for(state of states){
+    const city1 = await getLongestName(state.Sigla);
+    const city2 = await getSmallerName(state.Sigla);
+    resultList1.push({"Name": city1.Nome, "Size": city1.Nome.length, "UF": state.Sigla});
+    resultList2.push({"Name": city2.Nome, "Size": city2.Nome.length, "UF": state.Sigla});
+  }
+
+  resultList1.sort((a,b) => b.Size - a.Size);
+  resultList2.sort((a,b) => a.Size - b.Size);
+
+  let largestCity = resultList1[0];
+  let smallerCity = resultList2[0];
+
+  for(let i = 0; i < resultList1.length; i++){
+    if(resultList1[i].Size === largestCity.Size){
+      if(resultList1[i].Name.toLowerCase() < largestCity.Name.toLowerCase()){   //Compara ordem alfabetica
+        largestCity = resultList1[i];
+      }
+    }
+    if(resultList2[i].Size === smallerCity.Size){
+      if(resultList2[i].Name.toLowerCase() < smallerCity.Name.toLowerCase()){   //Compara ordem alfabetica
+        smallerCity = resultList2[i];
+      }
+    }
+  }
+
+  console.log("-- MAIOR NOME ENTRE TODOS OS ESTADOS");
+  console.log(largestCity.UF + " - " + largestCity.Name);
+
+  console.log("-- MENOR NOME ENTRE TODOS OS ESTADOS");
+  console.log(smallerCity.UF + " - " + smallerCity.Name); 
 }

d1a81ebc0340318c772022ccd9d11237fd5f16db
ramonis567 <ramonmatosferreira@gmail.com>
2020-12-02 16:20:26 -0300

    Final - Projeto Guiado 02

--- desafios-guiados-modulo03/desafio-guiado02/src/components/Country.js
+++ desafios-guiados-modulo03/desafio-guiado02/src/components/Country.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import css from "./countries.module.css";
 
 export default class Country extends Component {
   render() {
     const {country} = this.props;
 
     return (
-      <div>
-        {country.name}
+      <div className={`${css.country}`}>
+        <img 
+          src={country.flag} 
+          className={css.flagStyle} 
+          alt={`${country.name} flag`}>
+        </img>
+        <span>{country.name}</span>
       </div>
     );
   }
